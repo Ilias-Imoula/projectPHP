01924460c7640ffadcb53b1df1d52680f06c69df
ilias-imoula <iliasimoula97@gmail.com>
2020-01-10 13:52:04 +0100

    -a

---
+++ PageRévision.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<?php
+include('foot-head/header.php')
+?>
+<body>
+<form class="m-5">
+<div class="form-check form-check-inline">
+  <h1 class="form-check-label" for="inlineCheckbox1">1x</h1>
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+</div>
+<div class="form-check form-check-inline">
+  <h1 class="form-check-label" for="inlineCheckbox1">2x</h1>
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+</div>
+<div class="form-check form-check-inline">
+  <h1 class="form-check-label" for="inlineCheckbox1">3x</h1>
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+</div>
+<div class="form-check form-check-inline">
+  <h1 class="form-check-label" for="inlineCheckbox1">4x</h1>
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+</div>
+<div class="form-check form-check-inline">
+  <h1 class="form-check-label" for="inlineCheckbox1">5x</h1>
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+</div>
+<div class="form-check form-check-inline">
+  <h1 class="form-check-label" for="inlineCheckbox1">6x</h1>
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+</div>
+<div class="form-check form-check-inline">
+  <h1 class="form-check-label" for="inlineCheckbox1">7x</h1>
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+</div>
+<div class="form-check form-check-inline">
+  <h1 class="form-check-label" for="inlineCheckbox1">8x</h1>
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+</div>
+<div class="form-check form-check-inline">
+  <h1 class="form-check-label" for="inlineCheckbox1">9x</h1>
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+</div>
+</form>
+</body>
+<?php
+include('foot-head/footer.php')
+?>
+</html>
